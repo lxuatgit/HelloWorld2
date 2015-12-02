28a7349f21f6d8dd0ad464d9524e757190ba2f5f
lxuatgit <lei.xu@fhlbny.com>
2015-12-02 11:44:27 -0500

    commit command test

--- HelloWorld2/src/com/fhlbny/git/Hello.java
+++ HelloWorld2/src/com/fhlbny/git/Hello.java
@@ -3,6 +3,6 @@ package com.fhlbny.git;
 public class Hello {
 	public static void main (String[] args) 
 	{
-		System.out.println ("Hello World!");
+		System.out.println ("Hello World, change made!");
 	}
 }
